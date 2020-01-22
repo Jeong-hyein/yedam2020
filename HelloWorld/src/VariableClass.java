9a4fef46707697bdfe263b89f8010d29b819eebf
User <User@YD02-18>
2020-01-22 10:40:01 +0900

    0122 자바 예제

---
+++ HelloWorld/src/VariableClass.java
@@ -0,0 +1,12 @@
+
+public class VariableClass {
+	public static void main(String[] args) {
+		int sum, num1, num2, num3;
+		num1 = 100;
+		num2 = 90;
+		num3 = 80;
+		sum = num1 + num2 + num3;
+		System.out.println(sum);
+	}
+
+}
